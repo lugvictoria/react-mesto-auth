5bcb68c02249e13246a8256d55c62835addd5416
lugvictoria <victory.senty@yandex.ru>
2023-04-18 20:38:21 +0300

    12 спринт  изменение верстки Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,55 @@
-import logo from "../images/header-logo.svg";
+import logo from "../images/header__logo_light.svg";
+import React from "react";
 
-function Header() {
+function Header({ children, isWrappable }) {
+  let links;
+  if (children.length > 1) {
+    links = children;
+  } else {
+    links = [children];
+  }
+  const [isMenuOpened, setIsMenuOpened] = React.useState(false);
+  function handleOpenMenu() {
+    setIsMenuOpened((state) => !state);
+  }
   return (
-    <header className="header content__element content__element_type_header">
-      <img className="header__logo" src={logo} alt="Логотип Место" />
+    <header
+      className={
+        "header content__element content__element_type_header" +
+        (isWrappable ? " header_wrappable" : "")
+      }
+    >
+      <img
+        src={logo}
+        alt="Сервис Место-Россия. Логотип"
+        className="header__logo"
+      />
+      {isWrappable && (
+        <button
+          type="button"
+          className={
+            "header__menu-button" +
+            (isMenuOpened ? " header__menu-button_opened" : "")
+          }
+          aria-label="Открыть меню"
+          onClick={handleOpenMenu}
+        ></button>
+      )}
+
+      {links && (
+        <ul
+          className={
+            "header__menu" + (isMenuOpened ? " header__menu_opened" : "")
+          }
+        >
+          {[...links].map((item, pos) => (
+            <li className="header__menu-item" key={pos}>
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
     </header>
   );
 }
-
 export default Header;
