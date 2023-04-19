a4d0a2f123f9812ec31a61c324b14b91ef227ddc
lugvictoria <victory.senty@yandex.ru>
2023-04-19 12:32:49 +0300

    12 спринт создать управляемые компоненты в формах Register и Login

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,19 +1,44 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Register() {
+function Register({onSubmit}) {
+  const [inputs, setInputs] = React.useState({
+    email: '',
+    password: '',
+  });
+
+  function handleChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setInputs(state => ({...state, [name]: value}));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onSubmit(inputs);
+  }
+
   return (
     <div className="login content__element">
       <h2 className="login__title">Регистрация</h2>
-      <form className="login__form">
+      <form className="login__form" onSubmit={handleSubmit}>
         <input
           type="email"
           className="login__input"
           placeholder="Email"
+          name="email"
+          value={inputs.email}
+          onChange={handleChange}
+          required
         />
         <input
           type="password"
           className="login__input"
           placeholder="Пароль"
+          name="password"
+          value={inputs.password}
+          onChange={handleChange}
+          required
         />
         <button rype="submit" className="login__submit-button">Зарегистрироваться</button>
       </form>
