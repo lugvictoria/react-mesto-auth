67ad8ea46f9d5111f6bf36037bb02dfc54165248
lugvictoria <victory.senty@yandex.ru>
2023-04-19 17:42:09 +0300

    12 спринт роуты для разных страниц

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
 
   return (
     <footer className="footer content__element">
-      <p className="footer__copyright">&copy;&nbsp;{currentYear <= 2022 ? '2022' : `2022&ndash;${currentYear}`} Студенты Яндекс.Практикума</p>
+      <p className="footer__copyright">&copy;&nbsp;{currentYear <= 2023 ? "2023" : `2022&ndash;${currentYear}`} Яндекс.Практикум</p>
     </footer>
   );
 }
