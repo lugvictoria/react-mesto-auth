d12cbd1b6bb1c15aeed87e3c5ad6202623f9ef1e
lugvictoria <victory.senty@yandex.ru>
2023-04-20 12:15:16 +0300

    12 спринт перенести auth в папку utils

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,12 @@
 import React from "react";
-
 import logo from "../images/header__logo_light.svg";
 
 function Header({ children, isWrappable }) {
-  let links;
-  if (children.length > 1) {
-    links = children;
-  } else {
-    links = [children];
-  }
-
   const [isMenuOpened, setIsMenuOpened] = React.useState(false);
 
   function handleOpenMenu() {
     setIsMenuOpened((state) => !state);
   }
-
   return (
     <header
       className={
@@ -23,30 +14,31 @@ function Header({ children, isWrappable }) {
         (isWrappable ? " header_wrappable" : "")
       }
     >
-        <img
-          src={logo}
-          alt="Сервис Место-Россия. Логотип"
-          className="header__logo"
-        />
+      <img
+        src={logo}
+        alt="Сервис Место-Россия. Логотип"
+        className="header__logo"
+      />
+      {isWrappable && (
+        <button
+          type="button"
+          className={
+            "header__menu-button" +
+            (isMenuOpened ? " header__menu-button_opened" : "")
+          }
+          aria-label="Открыть меню"
+          onClick={handleOpenMenu}
+        ></button>
+      )}
 
-        {isWrappable && (
-          <button
-            type="button"
-            className={
-              "header__menu-button" +
-              (isMenuOpened ? " header__menu-button_opened" : "")
-            }
-            aria-label="Открыть меню"
-            onClick={handleOpenMenu}
-          ></button>
-        )}
-
-      {links && (
-        <nav className={
-          "header__menu" + (isMenuOpened ? " header__menu_opened" : "")
-        }>
+      {children && (
+        <nav
+          className={
+            "header__menu" + (isMenuOpened ? " header__menu_opened" : "")
+          }
+        >
           <ul className="header__menu-list">
-            {[...links].map((item, pos) => (
+            {(children.length > 1 ? children : [children]).map((item, pos) => (
               <li className="header__menu-item" key={pos}>
                 {item}
               </li>
