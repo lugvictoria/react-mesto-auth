9b0378bcbddd87706b635bf0b55213200614bfa4
lugvictoria <victory.senty@yandex.ru>
2023-04-20 13:41:06 +0300

    12 спринт корректировка footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ function Footer() {
   return (
     <footer className="footer content__element">
       <p className="footer__copyright">
-        &copy;&nbsp;{currentYear <= 2022 ? "2022" : `2022&ndash;${currentYear}`}{" "}
-        Студенты Яндекс.Практикума
+      &copy;&nbsp;{currentYear <= 2023 ? "2023" : `2022&ndash;${currentYear}`}{" "}
+        Mesto Russia
       </p>
     </footer>
   );
